2b955906d3f6c158c529a98b9128625a2e1009a0
Hamed Kharazmi <53815806+hamedkharazmi@users.noreply.github.com>
2021-12-02 14:53:10 +0330

    [ADD] Homework9 files to git

--- 09-serverless/lambda_function.py
+++ 09-serverless/lambda_function.py
@@ -4,7 +4,6 @@ from PIL import Image
 import numpy as np
 import tflite_runtime.interpreter as tflite
 
-
 def download_image(url):
     with request.urlopen(url) as resp:
         buffer = resp.read()
